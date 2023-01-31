2eb398479da17cd5b1b277b78989ea0e13259f0d
BartekMazurek <bartlomiejmazurek9@gmail.com>
2023-01-31 18:58:27 +0100

    Added java main

---
+++ java/src/Main.java
@@ -0,0 +1,9 @@
+import J1_data.types.J1_byteShortIntLong;
+import J1_data.types.J2_floatDouble;
+
+public class Main {
+    public static void main(String[] args) {
+        J1_byteShortIntLong.declare();
+        J2_floatDouble.declare();
+    }
+}
