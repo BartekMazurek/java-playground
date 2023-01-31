fc77a7577fb60b1f5b95c6cf9e16ad95beab5f55
BartekMazurek <bartlomiejmazurek9@gmail.com>
2023-01-31 18:58:19 +0100

    Added boolean & char example

---
+++ java/src/J1_data/types/J3_booleanChar.java
@@ -0,0 +1,11 @@
+package J1_data.types;
+
+public class J3_booleanChar {
+    public static void declare() {
+
+        boolean flag = true;
+
+        // Allows to assign one Unicode char (2 bytes), UTF-16 (65k available signs)
+        char sign = '\u0041';
+    }
+}
