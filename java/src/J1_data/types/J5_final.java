eade54d42b8b7e69da927258098180504139b1ed
BartekMazurek <bartlomiejmazurek9@gmail.com>
2023-02-01 21:00:22 +0100

    Added final constant example

---
+++ java/src/J1_data/types/J5_final.java
@@ -0,0 +1,9 @@
+package J1_data.types;
+
+public class J5_final {
+
+    public static void declare() {
+        // ALLOWED TO ASSIGN VALUE ONLY ONCE
+        final float constant = 2.55f;
+    }
+}
