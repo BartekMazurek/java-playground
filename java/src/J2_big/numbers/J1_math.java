a62ab593c43fd68a6bb93022726c89ebb787f575
BartekMazurek <bartlomiejmazurek9@gmail.com>
2023-02-06 21:24:12 +0100

    Added math example

---
+++ java/src/J2_big/numbers/J1_math.java
@@ -0,0 +1,17 @@
+package J2_big.numbers;
+
+public class J1_math {
+
+    public static void calculate() {
+
+        double number = 4.0d;
+        double squareRoot = Math.sqrt(number);
+
+        double power = Math.pow(10, 5);
+
+        double someValue = Math.abs(-10);
+        double someRandomValue = Math.random(); // >= 0 < 1
+
+        System.out.println("Some random value: " + someRandomValue);
+    }
+}
