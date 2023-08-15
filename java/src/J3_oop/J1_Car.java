804b5ba13928099b3b02ce168d1d05693025a68b
bartek <bartlomiejmazurek9@gmail.com>
2023-08-15 13:41:36 +0200

    Added overwritten method basic example

--- java/src/J3_oop/J1_Car.java
+++ java/src/J3_oop/J1_Car.java
@@ -34,4 +34,22 @@ public class J1_Car {
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
+
+    // OVERWRITTEN METHODS
+    // JAVA ALLOWS TO DEFINE MORE THAN ONE METHOD WITH SAME NAME
+    // METHODS SHOULD BE DIFFERENT (ARGUMENTS AMOUNT ETC.)
+    // EXACT SAME METHODS WILL TRIGGER COMPILATION ERROR
+    // JAVA WILL KNOW INTERNALLY WHAT METHOD SHOULD BE CALLED
+
+    public int overwrittenMethodExample(int a, int b) {
+        return a + b;
+    }
+
+    public float overwrittenMethodExample(float a, float b) {
+        return a + b;
+    }
+
+    public float overwrittenMethodExample(float a, float b, float c) {
+        return a + b + c;
+    }
 }
